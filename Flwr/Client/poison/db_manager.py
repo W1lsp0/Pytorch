8fd2c6d94d77743b29d8f197256d9fc3ecd83fb0
W1lsp0 <w1lsp0@foxmail.com>
2026-02-07 14:52:28 +0000

    再次修改

--- Flwr/Client/poison/db_manager.py
+++ Flwr/Client/poison/db_manager.py
@@ -34,7 +34,7 @@ class DBManager:
         - telemetry_logs: 动态运行时遥测
     """
 
-    def __init__(self, host="127.0.0.1", port=3306, user="root", password="root123456"):
+    def __init__(self, host="59.67.152.211", port=3306, user="root", password="root123456"):
         """
         初始化数据库管理器
         
